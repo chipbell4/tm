84840b159c3d3fc6d801a3cd9e2fde5f0653dbf9
Chip Bell <chip.bell@spinen.com>
2014-01-27 17:39:01 -0500

    Added a delete command

--- bin/tm-delete.js
+++ bin/tm-delete.js
@@ -5,9 +5,30 @@ exports.command = {
 };
 
 if(require.main === module) {
+	/*
+	 * Read in command line arguments
+	 */
+	var args = require('../lib/arguments.js');
 
 	/*
-	 * TODO: implement
+	 * Other libraries we'll need
 	 */
+	var FileResolver = require('../lib/file_resolver.js').FileResolver;
 
+	var template_full_path = FileResolver.resolve(args.template);
+
+	/*
+	 * If the file wasn't found, bail out
+	 */
+	if(template_full_path == null) {
+		console.log(FileResolver.errorMessage());
+		process.exit(0);
+	}
+
+	/*
+	 * Remove the file
+	 */
+	require('fs').unlink(template_full_path);
+
+	console.log("Removed template successfully");
 }
